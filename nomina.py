6982fc7770b5d7803e9537ed57c6e856b4ef2f37
David García Oriol <davidgarciaoriol@gmail.com>
2023-06-06 11:49:05 +0200

    second commit

---
+++ nomina.py
@@ -0,0 +1,21 @@
+class Nomina:
+
+    CC = 4.70
+    FP = 0.1
+    DP = 1.55
+
+    retencion = 0
+
+    salario_base = 0
+    complementos = []
+
+    def __init__(self, salario_base:int, complementos, retencion:int):
+        self.salario_base = salario_base
+        self.complementos = complementos
+        self.retencion = retencion
+
+    
+
+
+
+
