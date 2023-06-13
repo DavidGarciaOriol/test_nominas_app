df6a57093b9505a14e1bb197e44b290f3cf0e7f7
David García Oriol <davidgarciaoriol@gmail.com>
2023-06-13 11:58:33 +0200

    - - -

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from nomina import Nomina
 
-nomina1 = Nomina(salario_base=2000, complementos={"antigüedad"}, retencion=3, numero_pagas=14, antiguedad=True, horas_extra_normales=60, horas_extra_fuerza_mayor=30)
+nomina1 = Nomina(salario_base=2000, complementos={"productividad":10}, retencion=2, numero_pagas=14, antiguedad=False, horas_extra_normales=0, horas_extra_fuerza_mayor=0)
 
 
 print(nomina1)
